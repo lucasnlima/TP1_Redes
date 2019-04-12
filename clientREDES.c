90b6af0fa19d93d189a08a95da406440e95229cd
lucasnlima <47156806+lucasnlima@users.noreply.github.com>
2019-04-12 18:02:33 -0300

    Update clientREDES.c

--- clientREDES.c
+++ clientREDES.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
+#include <time.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -14,8 +14,10 @@ int main(int argc, char const *argv[]) {
         struct sockaddr_in adressServer;
         struct timeval tv;
 
-        int port, buffer_size;
+        int port, buffer_size, num_bytes=0;
         int sockfd, client;
+
+        clock_t tempo;
 
         //--- inteiro que armazena o tamanho da estrutra de endereço do cliente
         int csize = sizeof adressServer;
@@ -32,10 +34,13 @@ int main(int argc, char const *argv[]) {
         scanf("%d",&port);
         printf("\n\n Digite o tamanho do buffer:");
         scanf("%d",&buffer_size);
+        buffer_size = buffer_size*8;
 
         /*--Aqui temos a definição do nosso socket e os parementros quem definem
            AF_INET - Define que trabalhara com a internet, SOCK_STREAM - Uso do TCP
            0 - Defini o uso do IP*/
+
+
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
         adressServer.sin_family = AF_INET;
@@ -50,53 +55,55 @@ int main(int argc, char const *argv[]) {
                 printf("\n Socket Criado com sucesso");
 
         //-- Conecta ao Servidor
-        if(connect(sockfd, (struct sockaddr*) &adressServer, csize) == -1){
-            printf("\n Erro na conexão do socket");
-            exit(1);
+        if(connect(sockfd, (struct sockaddr*) &adressServer, csize) == -1) {
+                printf("\n Erro na conexão do socket");
+                exit(1);
         }else
-            printf("\n Conectado ao servidor");
+                printf("\n Conectado ao servidor");
 
-            memset(fileName,0x0,255);
+        memset(fileName,0x0,255);
 
         printf("\n\n Qual o nome do arquivo que deseja baixar:");
         scanf("%s",fileName);
 
-
-
+        tempo=clock();
 
         send(sockfd, fileName,strlen(fileName),0);
         printf("%s\n",fileName);
 
-                // -- cria o pronteiro de arquvo
-                FILE *arq;
+        // -- cria o pronteiro de arquvo
+        FILE *arq;
 
-                arq = fopen(fileName, "w+");
+        arq = fopen(fileName, "w+");
 
 
-                if(arq == NULL){
-                  printf("Erro na criação do arquivo");
-                  return 1;
-                }
+        if(arq == NULL) {
+                printf("Erro na criação do arquivo");
+                return 1;
+        }
 
-                char buffer[buffer_size];
-                memset(buffer,0,buffer_size);
+        char buffer[buffer_size];
+        memset(buffer,0,buffer_size);
 
-                while(recv(sockfd, buffer, buffer_size,0)>0){
-                //fputs(buffer, stdout);
+        while(recv(sockfd, buffer, buffer_size,0)>0) {
                 fprintf(arq, "%s", buffer);
                 memset(buffer,0,buffer_size);
-                }
+                printf("\rBytes já tranferidos: %d",num_bytes);
+                num_bytes += sizeof(arq);
+        }
+        num_bytes = sizeof(arq);
+        printf("\rBytes já tranferidos: %d",num_bytes);
 
-                fclose(arq);
 
-                printf("Arquivo tranferido com sucesso\n Conteudo: \n");
+        fclose(arq);
 
-                if(fputs(buffer, stdout) == EOF)
-                {
-                    printf("\nStandard output error");
-                }
+        printf("\nArquivo tranferido com sucesso.\nTempo gasto: %f segundos \n",(clock()-tempo)/(double)CLOCKS_PER_SEC);
+
+        if(fputs(buffer, stdout) == EOF)
+        {
+                printf("\nStandard output error");
+        }
 
 
         return 0;
 }
-
